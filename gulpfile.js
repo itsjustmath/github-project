f89bce675ca22bf25f64c544e23ebc7121ccf30b
Justin Mather <justin.mather@gmail.com>
2016-06-26 20:23:40 -0700

    building compiled primer to build directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('primer', function () {
+  return gulp.src('./node_modules/primer-css/build/build.css').pipe(gulp.dest('./build'));
+});
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sourcemaps.init())
+  return gulp.src('./sass/index.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./build'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.scss', ['primer', 'sass']);
 });
